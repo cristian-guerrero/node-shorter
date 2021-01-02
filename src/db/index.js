f12dccf2c80d3b5a0f211ebaa99bf258fc7a72b1
Cristian Guerrero <guerrero.chavez.cristian@gmail.com>
2021-01-02 18:22:29 -0500

    docker ignore modules

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,22 @@
 const { Pool, Client } = require('pg')
-const connectionString = 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB,
+  POSTGRES_DB_PORT, POSTGRES_DB_HOST, } = process.env
+
+//
+const connectionString = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_DB_HOST}:${POSTGRES_DB_PORT}/${POSTGRES_DB}`
 const pool = new Pool({
   connectionString,
 })
+
+
+/* 
+// official documentation https://node-postgres.com/features/connecting
+
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+*/
+
+module.exports = pool
